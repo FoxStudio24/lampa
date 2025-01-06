fcc5a4b824b34cebdd62ed9ef73e0a69a02ed402
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-01-06 14:24:32 +0200

    Add files via upload

---
+++ podborci.js
@@ -0,0 +1,405 @@
+{
+  "secuses": true,
+  "results": [
+    {
+      "hpu": "hbo.json",
+      "title": "Шедевры HBO",
+      "type": "movie",
+      "meta_title": "HBO",
+      "img": "img/hbo.png"
+    },
+    {
+      "hpu": "luchshie_horrory_a24.json",
+      "title": "Лучшие хорроры и триллеры А24",
+      "type": "movie",
+      "meta_title": "Лучшие хорроры и триллеры А24",
+      "img": "img/luchshie_horrory_a24.png"
+    },
+    {
+      "hpu": "samye_dobrye_filmy.json",
+      "title": "Самые добрые фильмы",
+      "type": "movie",
+      "meta_title": "Самые добрые фильмы",
+      "img": "img/samye_dobrye_filmy.png"
+    },
+    {
+      "hpu": "filmov_uzhasov.json",
+      "title": "Лучшие фильмы ужасов",
+      "type": "movie",
+      "meta_title": "Лучшие фильмы ужасов",
+      "img": "img/filmov_uzhasov.png"
+    },
+    {
+      "hpu": "poloviny_2024_goda.json",
+      "title": "Фильмы первой половины 2024 года",
+      "type": "movie",
+      "meta_title": "Фильмы первой половины 2024 года",
+      "img": "img/poloviny_2024_goda.png"
+    },
+    {
+      "hpu": "netflix_lyubov.json",
+      "title": "Сериалы Netflix про любовь",
+      "type": "movie",
+      "meta_title": "Сериалы Netflix про любовь",
+      "img": "img/netflix_lyubov.png"
+    },
+    {
+      "hpu": "serialy_supergeroev.json",
+      "title": "Сериалы про супергероев",
+      "type": "movie",
+      "meta_title": "Сериалы про супергероев",
+      "img": "img/serialy_supergeroev.png"
+    },
+    {
+      "hpu": "animacionn_vzroslyh.json",
+      "title": "Анимационные сериалы для взрослых",
+      "type": "movie",
+      "meta_title": "Анимационные сериалы для взрослых",
+      "img": "img/animacionn_vzroslyh.png"
+    },
+    {
+      "hpu": "filmy_sobakah.json",
+      "title": "Лучшие фильмы о собаках",
+      "type": "movie",
+      "meta_title": "Лучшие фильмы о собаках",
+      "img": "img/filmy_sobakah.png"
+    },
+    {
+      "hpu": "mark_uolberg.json",
+      "title": "Все фильмы с Марком Уолбергом",
+      "type": "movie",
+      "meta_title": "Все фильмы с Марком Уолбергом",
+      "img": "img/mark_uolberg.png"
+    },
+    {
+      "hpu": "multfilmy_disney.json",
+      "title": "Лучшие мультфильмы Disney",
+      "type": "movie",
+      "meta_title": "Мультфильмы Disney",
+      "img": "img/multfilmy_disney.png"
+    },
+    {
+      "hpu": "sem_comedy.json",
+      "title": "Семейные комедии",
+      "type": "movie",
+      "meta_title": "Семейные",
+      "img": "img/sem_comedy.png"
+    },
+    {
+      "hpu": "theme_kids_films.json",
+      "title": "Фильмы для детей",
+      "type": "movie",
+      "meta_title": "Лучшие фильмы для детей",
+      "img": "img/theme_kids_films.png"
+    },
+    {
+      "hpu": "serial_fox.json",
+      "title": "FOX",
+      "type": "movie",
+      "meta_title": "Сериалы FOX",
+      "img": "img/serial_fox.png"
+    },
+    {
+      "hpu": "netflix.json",
+      "title": "Netflix",
+      "type": "movie",
+      "meta_title": "Сериалы Netflix",
+      "img": "img/netflix.png"
+    },
+    {
+      "hpu": "dzheyson_steytem.json",
+      "title": "Брутальный Джейсон Стэйтем",
+      "type": "movie",
+      "meta_title": "Джейсон Стэйтем",
+      "img": "img/dzheyson_steytem.png"
+    },
+    {
+      "hpu": "nashestvie_monstrov.json",
+      "title": "Фильмы про нашествие монстров",
+      "type": "movie",
+      "meta_title": "Фильмы про нашествие монстров",
+      "img": "img/nashestvie_monstrov.png"
+    },
+    {
+      "hpu": "150serial.json",
+      "title": "150 лучших сериалов",
+      "type": "movie",
+      "meta_title": "150 Сериалов",
+      "img": "img/150serial.png"
+    },
+    {
+      "hpu": "francuzskih_komediy.json",
+      "title": "Лучшие французские комедий",
+      "type": "movie",
+      "meta_title": "Французские комедий",
+      "img": "img/francuzskih_komediy.png"
+    },
+    {
+      "hpu": "postapokalipsis.json",
+      "title": "Фильмы и сериалы про постапокалипсис",
+      "type": "movie",
+      "meta_title": "Постапокалипсис",
+      "img": "img/postapokalipsis.png"
+    },
+    {
+      "hpu": "marvel.json",
+      "title": "Фильмы и сериалы вселенной Marvel",
+      "type": "movie",
+      "meta_title": "Киновселенная MARVEL",
+      "img": "img/marvel.png"
+    },
+    {
+      "hpu": "film_manyak.json",
+      "title": "Лучшие фильмы про маньяков",
+      "type": "movie",
+      "meta_title": "Фильмы про маньяков",
+      "img": "img/film_manyak.png"
+    },
+    {
+      "hpu": "100anime.json",
+      "title": "100 лучших аниме-сериалов",
+      "type": "movie",
+      "meta_title": "Лучшие аниме-сериалы",
+      "img": "img/100anime.png"
+    },
+    {
+      "hpu": "po_versii_bbc.json",
+      "title": "100 лучших сериалов XXI века по версии BBC",
+      "type": "movie",
+      "meta_title": "100 лучших сериалов XXI века по версии BBC",
+      "img": "img/po_versii_bbc.png"
+    },
+    {
+      "hpu": "garri_potter.json",
+      "title": "Все фильмы о Гарри Поттере",
+      "type": "movie",
+      "meta_title": "Гарри Поттер",
+      "img": "img/garri_potter.png"
+    },
+    {
+      "hpu": "amerikanskie_komedii.json",
+      "title": "Лучшие американские комедии",
+      "type": "movie",
+      "meta_title": "Американские комедии",
+      "img": "img/amerikanskie_komedii.png"
+    },
+    {
+      "hpu": "robert_deniro.json",
+      "title": "Лучшие фильмы Роберта Де Ниро",
+      "type": "movie",
+      "meta_title": "Лучшие фильмы Роберта Де Ниро",
+      "img": "img/robert_deniro.png"
+    },
+    {
+      "hpu": "rozhdestvenskie_komedii.json",
+      "title": "Лучшие рождественские комедии",
+      "type": "movie",
+      "meta_title": "Рождественские комедии",
+      "img": "img/rozhdestvenskie_komedii.png"
+    },
+    {
+      "hpu": "seriynyh_ubiyc.json",
+      "title": "Сериалы про маньяков и серийных убийц",
+      "type": "movie",
+      "meta_title": "30 лучших сериалов про маньяков и серийных убийц",
+      "img": "img/seriynyh_ubiyc.png"
+    },
+    {
+      "hpu": "amerikanskie_filmy_80.json",
+      "title": "Лучшие американские фильмы 80-х и 90-х",
+      "type": "movie",
+      "meta_title": "Фильмы 80-х и 90-х",
+      "img": "img/amerikanskie_filmy_80.png"
+    },
+    {
+      "hpu": "redakcii_esquire.json",
+      "title": "Лучшие фильмы всех времен по версии Esquire",
+      "type": "movie",
+      "meta_title": "Лучшие фильмы всех времен",
+      "img": "img/redakcii_esquire.png"
+    },
+    {
+      "hpu": "filmy_gaya_richi.json",
+      "title": "Все фильмы Гая Ричи",
+      "type": "movie",
+      "meta_title": "Все фильмы Гая Ричи: от лучшего к худшему",
+      "img": "img/filmy_gaya_richi.png"
+    },
+    {
+      "hpu": "tureckie_melodramy.json",
+      "title": "Турецкие мелодрамы",
+      "type": "movie",
+      "meta_title": "Лучшие турецкие мелодрамы",
+      "img": "img/tureckie_melodramy.png"
+    },
+    {
+      "hpu": "britanskie_detektivnye.json",
+      "title": "Лучшие британские детективные сериалы",
+      "type": "movie",
+      "meta_title": "Британские детективные сериалы",
+      "img": "img/britanskie_detektivnye.png"
+    },
+    {
+      "hpu": "luchshie_sikvely.json",
+      "title": "Лучшие сиквелы",
+      "type": "movie",
+      "meta_title": "Сиквелы",
+      "img": "img/luchshie_sikvely.png"
+    },
+    {
+      "hpu": "neonuar_luchshie.json",
+      "title": "Неонуар",
+      "type": "movie",
+      "meta_title": "Неонуар: лучшие фильмы",
+      "img": "img/neonuar_luchshie.png"
+    },
+    {
+      "hpu": "skandalnye_amerikanskie.json",
+      "title": "Самые скандальные американские фильмы",
+      "type": "movie",
+      "meta_title": "Самые скандальные фильмы",
+      "img": "img/skandalnye_amerikanskie.png"
+    },
+    {
+      "hpu": "reytingom_r18.json",
+      "title": "Фильмы с рейтингом R",
+      "type": "movie",
+      "meta_title": "Самые кассовые фильмы с рейтингом R (18+)",
+      "img": "img/reytingom_r18.png"
+    },
+    {
+      "hpu": "misticheskie_serialy.json",
+      "title": "Лучшие мистические сериалы",
+      "type": "movie",
+      "meta_title": "Лучшие мистические сериалы",
+      "img": "img/misticheskie_serialy.png"
+    },
+    {
+      "hpu": "serialy_pro_vrachey.json",
+      "title": "Сериалы про врачей",
+      "type": "movie",
+      "meta_title": "Сериалы про врачей",
+      "img": "img/serialy_pro_vrachey.png"
+    },
+    {
+      "hpu": "serialy_pro_zavisimost.json",
+      "title": "Сериалы про зависимость",
+      "type": "movie",
+      "meta_title": "Сериалы про зависимость",
+      "img": "img/serialy_pro_zavisimost.png"
+    },
+    {
+      "hpu": "filmy_pro_spasenie_mira.json",
+      "title": "Фильмы про спасение мира",
+      "type": "movie",
+      "meta_title": "Фильмы про спасение мира",
+      "img": "img/filmy_pro_spasenie_mira.png"
+    },
+    {
+      "hpu": "filmy_pro_gangsterov.json",
+      "title": "Фильмы про гангстеров",
+      "type": "movie",
+      "meta_title": "Фильмы про гангстеров",
+      "img": "img/filmy_pro_gangsterov.png"
+    },
+    {
+      "hpu": "serialy_pro_zhenshchin.json",
+      "title": "Лучшие сериалы про женщин",
+      "type": "movie",
+      "meta_title": "Лучшие сериалы про женщин",
+      "img": "img/serialy_pro_zhenshchin.png"
+    },
+    {
+      "hpu": "pervyh_svidaniy.json",
+      "title": "Фильмы для первых свиданий",
+      "type": "movie",
+      "meta_title": "Фильмы для первых свиданий",
+      "img": "img/pervyh_svidaniy.png"
+    },
+    {
+      "hpu": "syurrealisticheskie_filmy.json",
+      "title": "Сюрреалистические фильмы",
+      "type": "movie",
+      "meta_title": "Лучшие сюрреалистические фильмы",
+      "img": "img/syurrealisticheskie_filmy.png"
+    },
+    {
+      "hpu": "konspirologicheskie_trillery.json",
+      "title": "Конспирологические триллеры",
+      "type": "movie",
+      "meta_title": "Лучшие Конспирологические триллеры",
+      "img": "img/konspirologicheskie_trillery.png"
+    },
+    {
+      "hpu": "filmy_stenli_kubrika.json",
+      "title": "Все фильмы Стэнли Кубрика",
+      "type": "movie",
+      "meta_title": "Все фильмы Стэнли Кубрика: от лучшего к хорошему",
+      "img": "img/filmy_stenli_kubrika.png"
+    },
+    {
+      "hpu": "maniacalniy_fincher.json",
+      "title": "Маниакальный перфекционист Дэвид Финчер",
+      "type": "movie",
+      "meta_title": "Все фильмы Дэвида Финчера",
+      "img": "img/maniacalniy_fincher.png"
+    },
+    {
+      "hpu": "amerikanskie_sitkomy.json",
+      "title": "Любимые американские ситкомы",
+      "type": "movie",
+      "meta_title": "Американские ситкомы",
+      "img": "img/amerikanskie_sitkomy.png"
+    },
+    {
+      "hpu": "arnold_shvarcenegger.json",
+      "title": "Фильмы с Арнольдом Шварценеггером",
+      "type": "movie",
+      "meta_title": "Арнольд Шварценеггер",
+      "img": "img/arnold_shvarcenegger.png"
+    },
+    {
+      "hpu": "amazon.json",
+      "title": "Сериалы Prime Video",
+      "type": "movie",
+      "meta_title": "Prime Video",
+      "img": "img/amazon.png"
+    },
+    {
+      "hpu": "finansovyh_mahinaciyah.json",
+      "title": "Фильмы о финансовых махинациях",
+      "type": "movie",
+      "meta_title": "Фильмы о финансовых махинациях",
+      "img": "img/finansovyh_mahinaciyah.png"
+    },
+    {
+      "hpu": "dueyna_skaly_dzhonsona.json",
+      "title": "Все фильмы Дуэйна «Скалы» Джонсона",
+      "type": "movie",
+      "meta_title": "Все фильмы Дуэйна «Скалы» Джонсона",
+      "img": "img/dueyna_skaly_dzhonsona.png"
+    },
+    {
+      "hpu": "luchshie_melodramy.json",
+      "title": "Лучшие мелодрамы XXI века",
+      "type": "movie",
+      "meta_title": "Лучшие мелодрамы",
+      "img": "img/luchshie_melodramy.png"
+    },
+    {
+      "hpu": "izmeny_zheny.json",
+      "title": "Фильмы про измены жены",
+      "type": "movie",
+      "meta_title": "Фильмы про измены",
+      "img": "img/izmeny_zheny.png"
+    },
+    {
+      "hpu": "kiberpank_kino.json",
+      "title": "Киберпанк в кино",
+      "type": "movie",
+      "meta_title": "Киберпанк",
+      "img": "img/kiberpank_kino.png"
+    }
+  ],
+  "total": 56
+}
