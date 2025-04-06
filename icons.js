3ab634f98c66195ce2191d08ee3c5fc133858f95
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-04-06 20:50:47 +0300

    Add files via upload

---
+++ icons.js
@@ -0,0 +1,25 @@
+!function() {
+    "use strict";
+
+    // Проверяем, что плагин ещё не инициализирован
+    if (!window.buttonplugin) {
+        window.buttonplugin = true;
+
+        // Подписываемся на событие полной загрузки интерфейса
+        Lampa.Listener.follow("full", function(a) {
+            if (a.type === "complite") {
+                // Находим кнопку "Онлайн"
+                var button = a.object.activity.render().find('.full-start__button.view--online');
+                if (button.length) {
+                    // Удаляем существующую SVG иконку
+                    button.find('svg').remove();
+                    
+                    // Добавляем PNG изображение перед текстом
+                    button.prepend(
+                        '<img style="width: 24px; height: 24px; margin-right: 5px; vertical-align: middle;" src="https://cdn-icons-png.flaticon.com/512/7476/7476981.png" />'
+                    );
+                }
+            }
+        });
+    }
+}();
