d2193bbee455569fa95a524401f5c650ee0bf681
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-04-06 22:27:01 +0300

    Update icons.js

--- icons.js
+++ icons.js
@@ -26,7 +26,6 @@
                 -webkit-backdrop-filter: blur(5px);
                 transition: all 0.3s ease;
                 overflow: hidden;
-                outline: none; /* Убираем стандартный outline */
             }
             .full-start__button span {
                 display: none;
@@ -36,20 +35,16 @@
             .full-start__button:hover,
             .full-start__button:focus,
             .full-start__button.active {
-                background-color: #fff !important;
-                color: #000 !important;
-                width: auto !important;
-                padding: 0.3em 1em !important;
-                border-radius: 9999em !important;
+                background-color: #fff;
+                color: #000;
+                width: auto;
+                padding: 0.3em 1em;
+                border-radius: 9999em;
             }
             .full-start__button:hover span,
             .full-start__button:focus span,
             .full-start__button.active span {
-                display: inline !important;
-            }
-            /* Для отладки: визуальный индикатор фокуса */
-            .full-start__button:focus {
-                box-shadow: 0 0 5px red !important; /* Красная обводка при фокусе */
+                display: inline;
             }
         `;
         document.head.appendChild(style);
@@ -59,22 +54,8 @@
                 var render = a.object.activity.render();
                 var iconSize = getIconSize();
 
-                // Находим все кнопки и добавляем tabindex + обработчики
-                var buttons = render.find('.full-start__button');
-                buttons.each(function() {
-                    var button = $(this);
-                    button.attr('tabindex', '0'); // Делаем кнопки фокусируемыми
-
-                    // Добавляем обработчик фокуса для отладки
-                    button.on('focus', function() {
-                        console.log('Кнопка в фокусе:', button.attr('class'));
-                        button.addClass('active'); // Принудительно добавляем .active
-                    });
-                    button.on('blur', function() {
-                        console.log('Кнопка потеряла фокус:', button.attr('class'));
-                        button.removeClass('active'); // Убираем .active при потере фокуса
-                    });
-                });
+                // Добавляем tabindex для всех кнопок
+                render.find('.full-start__button').attr('tabindex', '0');
 
                 // 1. Кнопка "Онлайн"
                 var onlineButton = render.find('.full-start__button.view--online');
@@ -120,7 +101,7 @@
                 var optionsButton = render.find('.full-start__button.button--options');
                 if (optionsButton.length) {
                     optionsButton.find('svg').remove();
-                    onlineButton.html(
+                    optionsButton.html(
                         '<img style="width: ' + iconSize + '; height: ' + iconSize + '; vertical-align: middle;" src="https://raw.githubusercontent.com/FoxStudio24/lampa/3f759f21cc988dbaf8c817d2d921ba535f416ace/icons/%D0%98%D0%98.svg" />' +
                         '<span>Опции</span>'
                     );
