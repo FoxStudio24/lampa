897ad4bd4fe56b7b43cc843d0b43fdfb2ce2e991
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-08-19 15:19:59 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,54 +1,96 @@
-!function() {  
-    "use strict";  
-      
-    // Функция загрузки внешнего скрипта  
-    function loadScript(url, callback) {  
-        var script = document.createElement('script');  
-        script.type = 'text/javascript';  
-        script.src = url;  
-        script.onload = callback || function() {};  
-        script.onerror = function() {  
-            console.error('Ошибка загрузки скрипта:', url);  
-        };  
-        document.head.appendChild(script);  
-    }  
-  
-    // Добавляем настройку в интерфейс  
-    Lampa.SettingsApi.addParam({  
-        component: "interface",  
-        param: {  
-            name: "foxstudio_interface",  
-            type: "select",  
-            values: { 1: "Выключен", 0: "Включен" },  
-            default: "1"  
-        },  
-        field: {  
-            name: "Новый интерфейс для тв и пк",  
-            description: "Включает плагины Necardify и Logo для улучшенного интерфейса"  
-        }  
-    });  
-  
-    // Отслеживаем изменения настройки  
-    Lampa.Storage.listener.follow('change', function(e) {  
-        if (e.name === 'foxstudio_interface') {  
-            if (e.value === '0') { // Включен  
-                // Загружаем оба плагина  
-                loadScript('https://foxstudio24.github.io/lampa/necardify.js', function() {  
-                    console.log('Necardify плагин загружен');  
-                });  
-                loadScript('https://foxstudio24.github.io/lampa/logo.js', function() {  
-                    console.log('Logo плагин загружен');  
-                });  
-            }  
-        }  
-    });  
-  
-    // Проверяем настройку при загрузке плагина  
-    if (Lampa.Storage.get('foxstudio_interface', '0') === '0') {  
-        loadScript('https://foxstudio24.github.io/lampa/necardify.js');  
-        loadScript('https://foxstudio24.github.io/lampa/logo.js');  
-    }  
-  
-    console.log('FoxStudio Interface Plugin загружен');  
-}();
+!function() {
+    "use strict";
+    Lampa.SettingsApi.addParam({
+        component: "interface",
+        param: {
+            name: "logo_glav",
+            type: "select",
+            values: { 1: "Скрыть", 0: "Отображать" },
+            default: "0"
+        },
+        field: {
+            name: "Логотипы вместо названий 2.0",
+            description: "Отображает логотипы фильмов и сериаллов вместо текста"
+        }
+    }),
+    window.logoplugin || (window.logoplugin = !0, Lampa.Listener.follow("full", (function(a) {
+        if ("complite" == a.type && "1" != Lampa.Storage.get("logo_glav")) {
+            var e = a.data.movie;
+            var isSerial = e.name || e.first_air_date;
+            var apiPath = isSerial ? "tv/" + e.id : "movie/" + e.id;
+            var t = Lampa.TMDB.api(apiPath + "/images?api_key=" + Lampa.TMDB.key());
+            console.log("API URL для логотипов:", t);
+            $.get(t, (function(e) {
+                if (e.logos && e.logos.length > 0) {
+                    console.log("Все логотипы:", e.logos);
+                    var logo = e.logos.find(function(l) { return l.iso_639_1 === "ru"; });
+                    var isRussianLogo = !!logo;
+                    if (!logo) {
+                        logo = e.logos.find(function(l) { return l.iso_639_1 === "en"; });
+                        console.log("Английский логотип:", logo ? "найден" : "не найден");
+                    }
+                    if (!logo) {
+                        logo = e.logos[0];
+                        console.log("Взят первый доступный логотип:", logo);
+                    }
+                    if (logo && logo.file_path) {
+                        var logoPath = Lampa.TMDB.image("/t/p/w300" + logo.file_path.replace(".svg", ".png"));
+                        console.log("Отображаем логотип:", logoPath);
 
+                        // Если логотип не русский, запрашиваем русское название
+                        if (!isRussianLogo) {
+                            var titleApi = Lampa.TMDB.api(apiPath + "?api_key=" + Lampa.TMDB.key() + "&language=ru");
+                            console.log("API URL для названия:", titleApi);
+                            $.get(titleApi, (function(data) {
+                                var russianTitle = isSerial ? data.name : data.title;
+                                console.log("Русское название из TMDB:", russianTitle);
+                                if (russianTitle) {
+                                    a.object.activity.render().find(".full-start-new__title").html(
+                                        '<div style="display: flex; flex-direction: column; align-items: flex-start; animation: fadeIn 0.9s ease-in;">' +
+                                            '<img style="margin-top: 5px; max-height: 125px;" src="' + logoPath + '" />' +
+                                            '<span style="margin-top: 5px; font-size: 32px; color: #fff;">' + russianTitle + '</span>' +
+                                        '</div>' +
+                                        '<style>' +
+                                            '@keyframes fadeIn {' +
+                                                'from { opacity: 0; }' +
+                                                'to { opacity: 1; }' +
+                                            '}' +
+                                        '</style>'
+                                    );
+                                } else {
+                                    a.object.activity.render().find(".full-start-new__title").html(
+                                        '<div style="display: flex; flex-direction: column; align-items: flex-start; animation: fadeIn 0.5s ease-in;">' +
+                                            '<img style="margin-top: 5px; max-height: 125px;" src="' + logoPath + '" />' +
+                                        '</div>' +
+                                        '<style>' +
+                                            '@keyframes fadeIn {' +
+                                                'from { opacity: 0; }' +
+                                                'to { opacity: 1; }' +
+                                            '}' +
+                                        '</style>'
+                                    );
+                                }
+                            }));
+                        } else {
+                            a.object.activity.render().find(".full-start-new__title").html(
+                                '<div style="display: flex; flex-direction: column; align-items: flex-start; animation: fadeIn 0.5s ease-in;">' +
+                                    '<img style="margin-top: 5px; max-height: 125px;" src="' + logoPath + '" />' +
+                                '</div>' +
+                                '<style>' +
+                                    '@keyframes fadeIn {' +
+                                        'from { opacity: 0; }' +
+                                        'to { opacity: 1; }' +
+                                    '}' +
+                                '</style>'
+                            );
+                        }
+                    } else {
+                        console.log("Логотип невалидный (нет file_path):", logo);
+                    }
+                } else {
+                    console.log("Логотипы отсутствуют для:", e.title || e.name);
+                }
+            }))
+        }
+    })))
+}();
