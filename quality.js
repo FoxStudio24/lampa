ca2dc4d5e36c224b9179d24248bf79fc3442d92b
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-09-08 22:07:14 +0300

    Update quality.js

--- quality.js
+++ quality.js
@@ -1,1 +1,33 @@
-
+(function() {  
+    'use strict';  
+  
+    Lampa.Plugin.add({  
+        component: 'card-quality',  
+        name: 'Card Quality Styles',  
+        description: 'Добавляет стили для отображения качества карточек',  
+        version: '1.0.1',  
+        author: 'lampac'  
+    });  
+  
+    // Обновленные CSS стили  
+    var css = `  
+       .card__quality {  
+            position: absolute;  
+            left: 10.2em;  
+            bottom: 2.5em;  
+            padding: 0.3em 0.5em;  
+            background: #ffffff;  
+            color: #000000;  
+            font-size: 1em;  
+            -webkit-border-radius: 0.3em;  
+            -moz-border-radius: 0.3em;  
+            border-radius: 0.9em;  
+        }  
+    `;  
+  
+    // Внедряем стили в документ  
+    var style = document.createElement('style');  
+    style.textContent = css;  
+    document.head.appendChild(style);  
+  
+})();
