3718d5ea38e913928624dc926ed09a6ac73e9c80
FoxStudio <133024877+FoxStudio24@users.noreply.github.com>
2025-09-11 18:04:40 +0300

    Update quality.js

--- quality.js
+++ quality.js
@@ -6,29 +6,54 @@
     function addCustomStyles() {    
         const style = document.createElement('style');    
         style.textContent = `    
-            .card__quality {    
-                position: absolute;    
-                right: 0.3em;    
-                top: 0.3em;    
-                padding: 0.3em 0.5em;    
-                background: #ffffff;    
-                color: #000000;    
-                font-size: 1em;    
-                -webkit-border-radius: 0.3em;    
-                -moz-border-radius: 0.3em;    
-                border-radius: 0.9em;    
-                width: auto;    
-                height: auto;    
-                display: inline-block;    
-                z-index: 2;    
-            }    
+            .card__quality {      
+                position: absolute;      
+                right: 0.3em;      
+                bottom: 3em;      
+                padding: 0.3em 0.5em;      
+                background: #ffffff;      
+                color: #000000;      
+                font-size: 1em;      
+                -webkit-border-radius: 0.3em;      
+                -moz-border-radius: 0.3em;      
+                border-radius: 0.9em;      
+            }  
+            .card__quality img {  
+                max-width: 100%;  
+                height: auto;  
+                display: block;  
+            }  
         `;    
         document.head.appendChild(style);    
-    }    
+    }  
+  
+    // Замена текста на изображения  
+    function replaceQualityText() {  
+        const qualityMap = {  
+            '4K': 'https://foxstudio24.github.io/lampa//quality/4к.png',  
+            'FLHD': 'https://foxstudio24.github.io/lampa//quality/FLHD.png',  
+            'TS': 'https://foxstudio24.github.io/lampa//quality/TS.png'  
+        };  
+  
+        const qualityElements = document.querySelectorAll('.card__quality');  
+        qualityElements.forEach(element => {  
+            const textContent = element.textContent.trim();  
+            if (qualityMap[textContent]) {  
+                element.innerHTML = `<img src="${qualityMap[textContent]}" alt="${textContent}">`;  
+            }  
+        });  
+    }  
         
     // Инициализация плагина    
     function init() {    
-        addCustomStyles();    
+        addCustomStyles();  
+        replaceQualityText();  
+          
+        // Наблюдатель за новыми элементами  
+        const observer = new MutationObserver(() => {  
+            replaceQualityText();  
+        });  
+        observer.observe(document.body, { childList: true, subtree: true });  
     }    
         
     // Запуск при загрузке    
